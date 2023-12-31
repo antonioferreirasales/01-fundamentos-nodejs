ef23aa4269cb300501f4f08c5e92e6d5241080b2
Antônio Sales <antoniosales1003@gmail.com>
2023-12-31 18:07:36 -0300

    feat: persistencia de dados

--- src/database.js
+++ src/database.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs/promises'
+const databasePath = new URL('db.json', import.meta.url)
+
 export class Database {
   #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf-8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select (table) {
     const date = this.#database[table] ?? []
@@ -13,7 +30,7 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
-    
+    this.#persist()
     return data;
   }
 
