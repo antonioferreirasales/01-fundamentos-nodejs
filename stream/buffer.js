43aaf44b1ada65512fb36a51b149e9a3e47f5ac3
Antônio Sales <antoniosales1003@gmail.com>
2023-12-31 12:29:21 -0300

    feat: buffer

---
+++ stream/buffer.js
@@ -0,0 +1,5 @@
+// Buffer -> representação de um espaço na memória do computador
+// * usado para transmitir dados rapidamente pois as armazena em sua forma binária
+const buf = Buffer.from('ok')
+
+console.log(buf)
