b24831f670e1ed2ec5e89066ea05a6c7214b234c
Antônio Sales <antoniosales1003@gmail.com>
2023-12-31 17:17:40 -0300

    feat: middleware

---
+++ src/middlewares/json.js
@@ -0,0 +1,16 @@
+// middlewares interceptam  e tratam algo
+export async function json(req, res) {
+  const buffer = []
+
+  for await( const chunk of req) {
+    buffer.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffer).toString())
+  } catch {
+    req.body = null
+  }
+
+  res.setHeader('Content-type', 'application/json')
+}
